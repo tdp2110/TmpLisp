d12ec1058be665ddc73ee9306077337d590ca23c
Tom Peters <thomas.d.peters@gmail.com>
2018-12-01 23:50:32 -0500

    representing Envs

--- test_tmp_lisp.cpp
+++ test_tmp_lisp.cpp
@@ -6,6 +6,19 @@ int main() {
   using Zero = IntConst<0>;
   using One = IntConst<1>;
   
-  static_assert(Eval<Zero, EmptyEnv>::value == 0);
-  static_assert(Eval<One, EmptyEnv>::value == 1);
+  static_assert(Eval<Zero, EmptyEnv> == 0);
+  static_assert(Eval<One, EmptyEnv> == 1);
+
+  using Var0 = Var<0>;
+  using Var1 = Var<1>;
+  using Var2 = Var<2>;
+  using MinusOne = IntConst<-1>;
+  using MinusTwo = IntConst<-2>;
+  using MinusThree = IntConst<-3>;
+  using TestEnv1 = Env<Binding<Var0, MinusOne>,
+                       Binding<Var1, MinusTwo>,
+                       Binding<Var2, MinusThree>>;
+  static_assert(Eval<Var0, TestEnv1> == MinusOne::value);
+  static_assert(Eval<Var1, TestEnv1> == MinusTwo::value);
+  static_assert(Eval<Var2, TestEnv1> == MinusThree::value);
 }
--- tmp_lisp.hpp
+++ tmp_lisp.hpp
@@ -9,15 +9,49 @@ namespace TmpLisp {
   template<int i>
   struct Var {};
 
-  struct EmptyEnv {};
+  template<class Variable, class Value>
+  struct Binding {};
+  
+  template<class ... Bindings>
+  struct Env;
+  
+  using EmptyEnv  = Env<>;
+
+  template<class Variable, class Env>
+  struct LookupImpl;
+
+  template<class Variable, class Value, class ... Bindings>
+  struct LookupImpl<Variable, Env<Binding<Variable, Value>, Bindings...>>
+  {
+    static constexpr auto value = Value::value;
+  };
+
+  template<class Variable, class Binding0, class ... Bindings>
+  struct LookupImpl<Variable, Env<Binding0, Bindings...>>
+  {
+    static constexpr auto value = LookupImpl<Variable, Env<Bindings...>>::value;
+  };
+  
+  template<class Variable, class Env>
+  constexpr auto Lookup =  LookupImpl<Variable, Env>::value;
   
   template<class Exp, class Env>
-  struct Eval;
+  struct EvalImpl;
 
   template<int i, class Env>
-  struct Eval<IntConst<i>, Env>
+  struct EvalImpl<IntConst<i>, Env>
   {
-    using IntType = IntConst<i>;
-    static constexpr int value = IntType::value;
+    using Int = IntConst<i>;
+    static constexpr auto value = Int::value;
   };
-}
+
+  template<int i, class Env>
+  struct EvalImpl<Var<i>, Env>
+  {
+    using Variable = Var<i>;
+    static constexpr auto value = Lookup<Variable, Env>;
+  };
+
+  template<class Exp, class Env>
+  constexpr auto Eval = EvalImpl<Exp, Env>::value;
+} // namespace TmpLisp
