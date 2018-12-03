aac06c05ee7749055923032928e8c7855d747f2b
Tom Peters <tom.peters@wrnch.ai>
2018-12-03 08:37:49 -0500

    clang format

--- test_tmp_lisp.cpp
+++ test_tmp_lisp.cpp
@@ -4,12 +4,17 @@
 
 using namespace TmpLisp;
 
+namespace {
+template <class T, class U>
+inline constexpr bool is_same_v = std::is_same<T, U>::value;
+}
+
 int main() {
   using Zero = IntConst<0>;
   using One = IntConst<1>;
   using Two = IntConst<2>;
   using Three = IntConst<3>;
-  
+
   static_assert(Eval_v<Zero, EmptyEnv> == 0);
   static_assert(Eval_v<One, EmptyEnv> == 1);
 
@@ -21,11 +26,9 @@ int main() {
   using MinusOne = IntConst<-1>;
   using MinusTwo = IntConst<-2>;
   using MinusThree = IntConst<-3>;
-  using TestEnv1 = Env<Binding<Var0, MinusOne>,
-                       Binding<Var1, MinusTwo>,
-                       Binding<Var2, MinusThree>,
-                       Binding<Var3, True>,
-                       Binding<Var4, False>>;
+  using TestEnv1 =
+      Env<Binding<Var0, MinusOne>, Binding<Var1, MinusTwo>,
+          Binding<Var2, MinusThree>, Binding<Var3, True>, Binding<Var4, False>>;
   static_assert(Eval_v<Var0, TestEnv1> == MinusOne::value);
   static_assert(Eval_v<Var1, TestEnv1> == MinusTwo::value);
   static_assert(Eval_v<Var2, TestEnv1> == MinusThree::value);
@@ -37,7 +40,7 @@ int main() {
   using TestIfExp2 = IfExp<Zero, Var1, Var2>;
   static_assert(Eval_v<TestIfExp2, TestEnv1> == Lookup_v<Var2, TestEnv1>);
   static_assert(Lookup_v<Var2, TestEnv1> == MinusThree::value);
-                
+
   using TestIfExp3 = IfExp<Var3, Var1, Var2>;
   static_assert(Eval_v<TestIfExp3, TestEnv1> == Lookup_v<Var1, TestEnv1>);
 
@@ -47,27 +50,21 @@ int main() {
   using TestIfExp5 = IfExp<TestIfExp4, TestIfExp3, TestIfExp2>;
   static_assert(Eval_v<TestIfExp5, TestEnv1> == Eval_v<TestIfExp3, TestEnv1>);
 
-  using TestLambda1 = Lambda<IfExp<Var0, Var1, Var2>,
-                             Env<Binding<Var1, One>>,
-                             Param<0>,
-                             Param<2>>;
+  using TestLambda1 = Lambda<IfExp<Var0, Var1, Var2>, Env<Binding<Var1, One>>,
+                             Param<0>, Param<2>>;
   using TestEnv2 = Env<Binding<Var3, Two>>;
-  static_assert(Eval_v<ApplicationExp<TestLambda1, Var3, Three>, TestEnv2> == 1);
-  static_assert(Eval_v<ApplicationExp<TestLambda1, False, Three>, TestEnv2> == 3);
+  static_assert(Eval_v<ApplicationExp<TestLambda1, Var3, Three>, TestEnv2> ==
+                1);
+  static_assert(Eval_v<ApplicationExp<TestLambda1, False, Three>, TestEnv2> ==
+                3);
 
-  static_assert(Eval_v<
-                ApplicationExp<Op<OpCode::Add>, Var0, Var1>,
-                Env<Binding<Var0, One>,
-                Binding<Var1, Two>>> == 1 + 2);
+  static_assert(Eval_v<ApplicationExp<Op<OpCode::Add>, Var0, Var1>,
+                       Env<Binding<Var0, One>, Binding<Var1, Two>>> == 1 + 2);
 
-  static_assert(Eval_v<
-                ApplicationExp<Op<OpCode::Mul>, Two, Three>,
-                Env<>> == 2 * 3);
+  static_assert(Eval_v<ApplicationExp<Op<OpCode::Mul>, Two, Three>, Env<>> ==
+                2 * 3);
 
-  static_assert(std::is_same_v<
-                  Eval<
-                    ApplicationExp<Op<OpCode::Eq>, Two, Three>,
-                  Env<>>::Result,
-                  False>);
-
+  static_assert(
+      is_same_v<Eval<ApplicationExp<Op<OpCode::Eq>, Two, Three>, Env<>>::Result,
+                False>);
 }
--- tmp_lisp.hpp
+++ tmp_lisp.hpp
@@ -1,257 +1,207 @@
 #pragma once
 
 namespace TmpLisp {
-  template<bool b>
-  struct BoolConst {
-    static constexpr bool value = b;
-  };
+template <bool b> struct BoolConst { static constexpr bool value = b; };
 
-  using True = BoolConst<true>;
-  using False = BoolConst<false>;
-  
-  template<int i>
-  struct IntConst {
-    static constexpr int value = i;
-  };
+using True = BoolConst<true>;
+using False = BoolConst<false>;
 
-  template<int i>
-  struct Var {};
+template <int i> struct IntConst { static constexpr int value = i; };
 
-  template<class Variable, class Value>
-  struct Binding {};
-  
-  template<class ... Bindings>
-  struct Env {};
+template <int i> struct Var {};
 
-  using EmptyEnv  = Env<>;
+template <class Variable, class Value> struct Binding {};
 
-  namespace detail {
-    template<class T0, class T1>
-    struct Concat;
+template <class... Bindings> struct Env {};
 
-    template<class ... Bindings1, class ... Bindings2>
-    struct Concat<Env<Bindings1...>, Env<Bindings2...>>
-    {
-      using Result = Env<Bindings1..., Bindings2...>;
-    };
+using EmptyEnv = Env<>;
 
-    template<class ... Ts>
-    struct List {};
-    
-    template<class, class>
-    struct MakeEnv;
+namespace detail {
+template <class T0, class T1> struct Concat;
 
-    template<>
-    struct MakeEnv<List<>, List<>>
-    {
-      using Result = EmptyEnv;
-    };
+template <class... Bindings1, class... Bindings2>
+struct Concat<Env<Bindings1...>, Env<Bindings2...>> {
+  using Result = Env<Bindings1..., Bindings2...>;
+};
 
-    template<class Variable, class ... Variables, class Value, class ... Values>
-    struct MakeEnv<List<Variable, Variables ...>, List<Value, Values ...>>
-    {
-      static_assert(sizeof...(Variables) == sizeof...(Values));
-      using InitialEnv = Env<Binding<Variable, Value>>;
-      using FinalEnv = typename MakeEnv<List<Variables...>, List<Values...>>::Result;
-      using Result = typename Concat<InitialEnv, FinalEnv>::Result;
-    };
-  }
+template <class... Ts> struct List {};
 
-  template<class Variables, class Values>
-  using MakeEnv_t = typename detail::MakeEnv<Variables, Values>::Result;
+template <class, class> struct MakeEnv;
 
-  template<class Env1, class Env2>
-  using ExtendEnv_t = typename detail::Concat<Env1, Env2>::Result;
-  
-  template<class Variable, class Env>
-  struct Lookup;
+template <> struct MakeEnv<List<>, List<>> { using Result = EmptyEnv; };
 
-  template<class Variable, class Value, class ... Bindings>
-  struct Lookup<Variable, Env<Binding<Variable, Value>, Bindings...>>
-  {
-    using Result = Value;
-    static constexpr auto value = Result::value;
-  };
+template <class Variable, class... Variables, class Value, class... Values>
+struct MakeEnv<List<Variable, Variables...>, List<Value, Values...>> {
+  static_assert(sizeof...(Variables) == sizeof...(Values));
+  using InitialEnv = Env<Binding<Variable, Value>>;
+  using FinalEnv =
+      typename MakeEnv<List<Variables...>, List<Values...>>::Result;
+  using Result = typename Concat<InitialEnv, FinalEnv>::Result;
+};
+} // namespace detail
 
-  enum class OpCode
-  {
-    Add,
-    Sub,
-    Mul,
-    Eq,
-  };
+template <class Variables, class Values>
+using MakeEnv_t = typename detail::MakeEnv<Variables, Values>::Result;
 
-  template<OpCode op>
-  struct Op {};
-  
-  template<class OperatorExp, class ... OperandExps>
-  struct ApplicationExp {};
-  
-  template<int i>
-  using Param = Var<i>;
+template <class Env1, class Env2>
+using ExtendEnv_t = typename detail::Concat<Env1, Env2>::Result;
 
-  template<class Body, class Environment, class ... Params>
-  struct Lambda {};
-  
-  template<class T>
-  using Result_t = typename T::Result;
-  
-  template<class Variable, class Binding0, class ... Bindings>
-  struct Lookup<Variable, Env<Binding0, Bindings...>>
-  {
-    using Result = Result_t<Lookup<Variable, Env<Bindings...>>>;
-    static constexpr auto value = Result::value;
-  };
-  
-  template<class Variable, class Env>
-  constexpr auto Lookup_v =  Lookup<Variable, Env>::value;
+template <class Variable, class Env> struct Lookup;
 
-  template<class Variable, class Env>
-  using Lookup_t =  typename Lookup<Variable, Env>::Result;
+template <class Variable, class Value, class... Bindings>
+struct Lookup<Variable, Env<Binding<Variable, Value>, Bindings...>> {
+  using Result = Value;
+  static constexpr auto value = Result::value;
+};
 
-  template<class Cond, class IfTrue, class IfFalse>
-  struct IfExp {};
+enum class OpCode {
+  Add,
+  Sub,
+  Mul,
+  Eq,
+};
 
-  template<class Operator, class ... Operands>
-  struct Apply;
+template <OpCode op> struct Op {};
 
-  /*****************
-        EVAL
-   *****************/
-  
-  template<class Exp, class Env>
-  struct Eval;
+template <class OperatorExp, class... OperandExps> struct ApplicationExp {};
 
-  template<int i, class Env>
-  struct Eval<IntConst<i>, Env>
-  {
-    using Result = IntConst<i>;
-    static constexpr auto value = Result::value;
-  };
+template <int i> using Param = Var<i>;
 
-  template<bool b, class Env>
-  struct Eval<BoolConst<b>, Env>
-  {
-    using Result = BoolConst<b>;
-    static constexpr auto value = Result::value;
-  };  
-  
-  template<int i, class Env>
-  struct Eval<Var<i>, Env>
-  {
-    using Result = Lookup_t<Var<i>, Env>;
-    static constexpr auto value = Result::value;
-  };
+template <class Body, class Environment, class... Params> struct Lambda {};
 
-  template<class Cond, class IfTrue, class IfFalse, class Env>
-  struct Eval<IfExp<Cond, IfTrue, IfFalse>, Env>;
+template <class T> using Result_t = typename T::Result;
 
-  template <class IfTrue, class IfFalse, class Env>
-  struct Eval<IfExp<True, IfTrue, IfFalse>, Env>
-  {
-    using Result = Result_t<Eval<IfTrue, Env>>;
-    static constexpr auto value = Result::value;
-  };
-  
-  template <class IfTrue, class IfFalse, class Env>
-  struct Eval<IfExp<False, IfTrue, IfFalse>, Env>
-  {
-    using Result = Result_t<Eval<IfFalse, Env>>;
-    static constexpr auto value = Result::value;
-  };
-  
-  template <int i, class IfTrue, class IfFalse, class Env>
-  struct Eval<IfExp<IntConst<i>, IfTrue, IfFalse>, Env>
-  {
-    using Result = Result_t<Eval<IfTrue, Env>>;
-    static constexpr auto value = Result::value;
-  };
-  
-  template <class IfTrue, class IfFalse, class Env>
-  struct Eval<IfExp<IntConst<0>, IfTrue, IfFalse>, Env>
-  {
-    using Result = Result_t<Eval<IfFalse, Env>>;
-    static constexpr auto value = Result::value;
-  };
+template <class Variable, class Binding0, class... Bindings>
+struct Lookup<Variable, Env<Binding0, Bindings...>> {
+  using Result = Result_t<Lookup<Variable, Env<Bindings...>>>;
+  static constexpr auto value = Result::value;
+};
 
-  template<class Cond, class IfTrue, class IfFalse, class Env>
-  struct Eval<IfExp<Cond, IfTrue, IfFalse>, Env>
-  {
-    using Result = Result_t<Eval<
-                              IfExp<
-                                Result_t<Eval<Cond, Env>>,
-                                IfTrue,
-                                IfFalse>,
-                              Env>>;
-    static constexpr auto value = Result::value;
-  };
-  
-  template<class Body, class LambdaEnv, class ... Params, class Env>
-  struct Eval<Lambda<Body, LambdaEnv, Params...>, Env>
-  {
-    using Result = Lambda<Body, ExtendEnv_t<LambdaEnv, Env>, Params...>;
-    static constexpr auto value = Result();
-  };
+template <class Variable, class Env>
+constexpr auto Lookup_v = Lookup<Variable, Env>::value;
 
-  template<class OperatorExp, class ... OperandExps, class Env>
-  struct Eval<ApplicationExp<OperatorExp, OperandExps...>, Env>
-  {
-    using Result = typename Apply<typename Eval<OperatorExp, Env>::Result,
-                                  typename Eval<OperandExps, Env>::Result...>::Result;
-    static constexpr auto value = Result::value;
-  };
+template <class Variable, class Env>
+using Lookup_t = typename Lookup<Variable, Env>::Result;
 
-  template<OpCode opcode, class ... OperandExps, class Env>
-  struct Eval<ApplicationExp<Op<opcode>, OperandExps...>, Env>
-  {
-    using Result = typename Apply<Op<opcode>,
-                                  typename Eval<OperandExps, Env>::Result...>::Result;
-    static constexpr auto value = Result::value;
-  };
-  
-  template<class Exp, class Env>
-  constexpr auto Eval_v = Eval<Exp, Env>::value;
+template <class Cond, class IfTrue, class IfFalse> struct IfExp {};
 
-  /*****************
-        APPLY
-   *****************/
+template <class Operator, class... Operands> struct Apply;
 
-  template<int i1, int i2>
-  struct Apply<Op<OpCode::Add>, IntConst<i1>, IntConst<i2>>
-  {
-    using Result = IntConst<i1 + i2>;
-    static constexpr auto value = Result::value;
-  };
-  
-  template<int i1, int i2>
-  struct Apply<Op<OpCode::Sub>, IntConst<i1>, IntConst<i2>>
-  {
-    using Result = IntConst<i1 - i2>;
-    static constexpr auto value = Result::value;
-  };
-  
-  template<int i1, int i2>
-  struct Apply<Op<OpCode::Mul>, IntConst<i1>, IntConst<i2>>
-  {
-    using Result = IntConst<i1 * i2>;
-    static constexpr auto value = Result::value;
-  };
-  
-  template<int i1, int i2>
-  struct Apply<Op<OpCode::Eq>, IntConst<i1>, IntConst<i2>>
-  {
-    using Result = BoolConst<i1 == i2>;
-    static constexpr auto value = Result::value;
-  };
-  
-  template<class BodyExp, class LambdaEnv, class ... Params, class ... Args>
-  struct Apply<Lambda<BodyExp, LambdaEnv, Params...>, Args ...> {
-    static_assert(sizeof...(Params) == sizeof...(Args));
-    using ExtendedEnv = ExtendEnv_t<LambdaEnv,
-                                    MakeEnv_t<detail::List<Params ...>,
-                                              detail::List<Args ...>>>;
-    using Result = typename Eval<BodyExp, ExtendedEnv>::Result;
-    static constexpr auto value = Result::value;
-  };
-  
+/*****************
+      EVAL
+ *****************/
+
+template <class Exp, class Env> struct Eval;
+
+template <int i, class Env> struct Eval<IntConst<i>, Env> {
+  using Result = IntConst<i>;
+  static constexpr auto value = Result::value;
+};
+
+template <bool b, class Env> struct Eval<BoolConst<b>, Env> {
+  using Result = BoolConst<b>;
+  static constexpr auto value = Result::value;
+};
+
+template <int i, class Env> struct Eval<Var<i>, Env> {
+  using Result = Lookup_t<Var<i>, Env>;
+  static constexpr auto value = Result::value;
+};
+
+template <class Cond, class IfTrue, class IfFalse, class Env>
+struct Eval<IfExp<Cond, IfTrue, IfFalse>, Env>;
+
+template <class IfTrue, class IfFalse, class Env>
+struct Eval<IfExp<True, IfTrue, IfFalse>, Env> {
+  using Result = Result_t<Eval<IfTrue, Env>>;
+  static constexpr auto value = Result::value;
+};
+
+template <class IfTrue, class IfFalse, class Env>
+struct Eval<IfExp<False, IfTrue, IfFalse>, Env> {
+  using Result = Result_t<Eval<IfFalse, Env>>;
+  static constexpr auto value = Result::value;
+};
+
+template <int i, class IfTrue, class IfFalse, class Env>
+struct Eval<IfExp<IntConst<i>, IfTrue, IfFalse>, Env> {
+  using Result = Result_t<Eval<IfTrue, Env>>;
+  static constexpr auto value = Result::value;
+};
+
+template <class IfTrue, class IfFalse, class Env>
+struct Eval<IfExp<IntConst<0>, IfTrue, IfFalse>, Env> {
+  using Result = Result_t<Eval<IfFalse, Env>>;
+  static constexpr auto value = Result::value;
+};
+
+template <class Cond, class IfTrue, class IfFalse, class Env>
+struct Eval<IfExp<Cond, IfTrue, IfFalse>, Env> {
+  using Result =
+      Result_t<Eval<IfExp<Result_t<Eval<Cond, Env>>, IfTrue, IfFalse>, Env>>;
+  static constexpr auto value = Result::value;
+};
+
+template <class Body, class LambdaEnv, class... Params, class Env>
+struct Eval<Lambda<Body, LambdaEnv, Params...>, Env> {
+  using Result = Lambda<Body, ExtendEnv_t<LambdaEnv, Env>, Params...>;
+  static constexpr auto value = Result();
+};
+
+template <class OperatorExp, class... OperandExps, class Env>
+struct Eval<ApplicationExp<OperatorExp, OperandExps...>, Env> {
+  using Result =
+      typename Apply<typename Eval<OperatorExp, Env>::Result,
+                     typename Eval<OperandExps, Env>::Result...>::Result;
+  static constexpr auto value = Result::value;
+};
+
+template <OpCode opcode, class... OperandExps, class Env>
+struct Eval<ApplicationExp<Op<opcode>, OperandExps...>, Env> {
+  using Result =
+      typename Apply<Op<opcode>,
+                     typename Eval<OperandExps, Env>::Result...>::Result;
+  static constexpr auto value = Result::value;
+};
+
+template <class Exp, class Env> constexpr auto Eval_v = Eval<Exp, Env>::value;
+
+/*****************
+      APPLY
+ *****************/
+
+template <int i1, int i2>
+struct Apply<Op<OpCode::Add>, IntConst<i1>, IntConst<i2>> {
+  using Result = IntConst<i1 + i2>;
+  static constexpr auto value = Result::value;
+};
+
+template <int i1, int i2>
+struct Apply<Op<OpCode::Sub>, IntConst<i1>, IntConst<i2>> {
+  using Result = IntConst<i1 - i2>;
+  static constexpr auto value = Result::value;
+};
+
+template <int i1, int i2>
+struct Apply<Op<OpCode::Mul>, IntConst<i1>, IntConst<i2>> {
+  using Result = IntConst<i1 * i2>;
+  static constexpr auto value = Result::value;
+};
+
+template <int i1, int i2>
+struct Apply<Op<OpCode::Eq>, IntConst<i1>, IntConst<i2>> {
+  using Result = BoolConst<i1 == i2>;
+  static constexpr auto value = Result::value;
+};
+
+template <class BodyExp, class LambdaEnv, class... Params, class... Args>
+struct Apply<Lambda<BodyExp, LambdaEnv, Params...>, Args...> {
+  static_assert(sizeof...(Params) == sizeof...(Args));
+  using ExtendedEnv =
+      ExtendEnv_t<LambdaEnv,
+                  MakeEnv_t<detail::List<Params...>, detail::List<Args...>>>;
+  using Result = typename Eval<BodyExp, ExtendedEnv>::Result;
+  static constexpr auto value = Result::value;
+};
+
 } // namespace TmpLisp
