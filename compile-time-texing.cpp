aab6aa187a577084cbd8cc4a7f170e0eeb334b6d
Tom Peters <thomas.d.peters@gmail.com>
2019-01-29 08:22:23 -0500

    playing with boost hana

--- compile-time-texing.cpp
+++ compile-time-texing.cpp
@@ -28,24 +28,33 @@ struct Word {};
 struct Number {};
 struct LexError {};
 
+enum class TokenType { Word, Number, LexError, StopSymbol };
+
+template <TokenType> struct Token {};
+
 namespace hana = boost::hana;
+
+static constexpr auto word_regex = "^([A-Za-z]+)"_ctre;
+static constexpr auto number_regex = "^([\\d]+)"_ctre;
 
 template <class LexedSoFar>
 static constexpr auto lexme_(std::string_view s, LexedSoFar lexed_so_far) {
   if (s.size() == 0) {
-    return lexed_so_far;
+    return hana::append(lexed_so_far, TokenType::StopSymbol);
   }
-  if (auto [re, m] = "^([A-Za-z]+)"_ctre.search(s); re) {
-    return lexme_(s.substr(m.size()), hana::append(lexed_so_far, Word()));
+  if (auto [re, m] = word_regex.search(s); re) {
+    return lexme_(s.substr(m.size()),
+                  hana::append(lexed_so_far, TokenType::Word));
   }
-  if (auto [re, m] = "^([\\d]+)"_ctre.search(s); re) {
-    return lexme_(s.substr(m.size()), hana::append(lexed_so_far, Number()));
+  if (auto [re, m] = number_regex.search(s); re) {
+    return lexme_(s.substr(m.size()),
+                  hana::append(lexed_so_far, TokenType::Number));
   }
-  return hana::append(lexed_so_far, LexError());
+  return hana::append(lexed_so_far, TokenType::LexError);
 }
 
 static constexpr auto lexme(std::string_view s) {
-  return lexme_(s, hana::make_tuple());
+  return lexme_(s, hana::make<hana::basic_tuple_tag>());
 }
 
 int main() {
