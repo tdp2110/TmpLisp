151600aec2d6e3f26cea62f73518f636d7469ded
Tom Peters <thomas.d.peters@gmail.com>
2019-01-28 22:49:17 -0500

    blowin' up the compiler with boost::hana

--- compile-time-texing.cpp
+++ compile-time-texing.cpp
@@ -1,4 +1,5 @@
 #include "ThirdParty/ctre.hpp"
+#include <boost/hana.hpp>
 
 #include <iostream>
 #include <optional>
@@ -23,6 +24,30 @@ static constexpr std::optional<std::string_view> lexit(std::string_view s) {
   return std::nullopt;
 }
 
+struct Word {};
+struct Number {};
+struct LexError {};
+
+namespace hana = boost::hana;
+
+template <class LexedSoFar>
+static constexpr auto lexme_(std::string_view s, LexedSoFar lexed_so_far) {
+  if (s.size() == 0) {
+    return lexed_so_far;
+  }
+  if (auto [re, m] = "^([A-Za-z]+)"_ctre.search(s); re) {
+    return lexme_(s.substr(m.size()), hana::append(lexed_so_far, Word()));
+  }
+  if (auto [re, m] = "^([\\d]+)"_ctre.search(s); re) {
+    return lexme_(s.substr(m.size()), hana::append(lexed_so_far, Number()));
+  }
+  return hana::append(lexed_so_far, LexError());
+}
+
+static constexpr auto lexme(std::string_view s) {
+  return lexme_(s, hana::make_tuple());
+}
+
 int main() {
   {
     constexpr std::string_view s{"abc123"};
@@ -31,6 +56,8 @@ int main() {
 
     std::cout << *res << "\n";
   }
+
+  lexme("abc123");
 
   return 0;
 }
