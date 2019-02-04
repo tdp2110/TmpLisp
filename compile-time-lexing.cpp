e41d7687cecdb2555755847d1d16a3c3573bf113
Tom Peters <thomas.d.peters@gmail.com>
2019-02-03 22:12:57 -0500

    working on lexing recursion

--- compile-time-lexing.cpp
+++ compile-time-lexing.cpp
@@ -48,9 +48,7 @@ template <auto &input, size_t n> struct SliceFirst {
   static constexpr auto _input = input;
   static_assert(n < input.size());
   static constexpr size_t resLength = input.size() - n;
-
   static constexpr auto prefix_string = prefix_slice_string<_input, n>();
-
   static constexpr auto res = ctll::basic_fixed_string{prefix_string.content};
 };
 
@@ -60,30 +58,44 @@ static constexpr auto number_regex = "^([\\d]+)"_ctre;
 static constexpr std::optional<std::string_view>
 MatchesWord(std::string_view s) noexcept {
   if (auto [re, m] = word_regex.search(s); re) {
-    return s.substr(m.size());
+    return s.substr(0, m.size());
   }
-
   return std::nullopt;
 }
 
+static constexpr int MatchSize(std::string_view s) noexcept {
+  if (auto [re, m] = word_regex.search(s); re) {
+    return m.size();
+  }
+  return -1;
+}
+
+template <bool HasValue> struct MaybeSize {
+  static constexpr int Call(std::optional<std::string_view> s) {
+    return s->size();
+  }
+};
+
+template <> struct MaybeSize<false> {
+  static constexpr int Call(std::optional<std::string_view>) { return 0; }
+};
+
 template <auto &input, class LexedSoFar> struct Lexer {
   static constexpr auto _input = input;
-  static constexpr auto matchedWord = MatchesWord(_input.begin());
+  static constexpr auto matchedWord =
+      MatchesWord(std::string_view(_input.begin(), _input.size()));
 
-  using tokens = decltype(matchedWord);
-
-  /*
-using tokens = std::conditional_t<
-    matchedWord.has_value(),
-    typename Lexer<matchedWord.value(),
-                   decltype(ctll::push_front(
-                       std::declval<Word>(),
-                       std::declval<LexedSoFar>()))>::results,
-    std::conditional_t<_input.size() == 0, LexedSoFar,
-                       decltype(
-                           ctll::push_front(std::declval<LexError>(),
-                                            std::declval<LexedSoFar>()))>>;
-  */
+  using tokens = std::conditional_t<
+      matchedWord.has_value(),
+      typename Lexer<
+          SliceFirst<_input, MaybeSize<matchedWord.has_value()>::Call(
+                                 matchedWord)>::res,
+          decltype(ctll::push_front(std::declval<Word>(),
+                                    std::declval<LexedSoFar>()))>::tokens,
+      std::conditional_t<_input.size() == 0, LexedSoFar,
+                         decltype(
+                             ctll::push_front(std::declval<LexError>(),
+                                              std::declval<LexedSoFar>()))>>;
 };
 
 int main() {
@@ -97,11 +109,6 @@ int main() {
 
   static_assert(MatchesWord(subject.begin()).has_value());
 
-  static constexpr auto s2 = ctll::basic_fixed_string{"asdf"};
-  // constexpr auto lexed = Lex<s2>();
-
-  using tokens = typename Lexer<s2, ctll::empty_list>::tokens;
-
   static constexpr auto s_str = ctll::basic_fixed_string{"frankiedog"};
 
   {
@@ -112,6 +119,36 @@ int main() {
   {
     constexpr auto slice = SliceFirst<s_str, 2>::res;
     static_assert(slice[0] == 'a');
+  }
+
+  {
+    static constexpr auto str = ctll::basic_fixed_string{"ab cde"};
+    static_assert(str.size() == 6);
+
+    static constexpr int matchSize = MatchSize(str.begin());
+    static_assert(matchSize == 2);
+
+    static constexpr auto slice = std::string_view(str.begin()).substr(2);
+    static_assert(slice.size() == 4);
+    static_assert(slice[0] == ' ');
+
+    static constexpr auto match1 = MatchesWord(str.begin());
+    static_assert(match1.has_value());
+    static_assert(match1->size() == 2);
+    static constexpr auto first = SliceFirst<str, match1->size()>::res;
+
+    static_assert(first.size() == 4);
+    static_assert(first[0] == ' ');
+
+    // static_assert(MatchSize(first.begin()) == 0);
+    static constexpr auto s = std::string_view(first.begin(), first.size());
+
+    // static constexpr auto match2 = MatchesWord(first.begin());
+    // static_assert(match2.has_value());
+
+    // static constexpr auto prefix2 = SliceFirst<str, match2->size()>::res;
+
+    using tokens = typename Lexer<str, ctll::empty_list>::tokens;
   }
 
   return 0;
